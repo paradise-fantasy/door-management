025b697fd82ac21213d8c9969a44bb52d94d46f5
freddyny <frederikny@gmail.com>
2016-09-14 18:40:19 +0200

    host server online

--- rest-api.py
+++ rest-api.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
+#import unlocker
 
 app = Flask(__name__)
 
 @app.route('/api/open-door', methods=['GET'])
 def get_tasks():
-#    return jsonify({'tasks': tasks})
+    #unlock()
     return "hello world"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
